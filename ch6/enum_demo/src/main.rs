dc678e794d9706aa9a9392342286862193c67cbf
huguanting.frank <huguanting.frank@bytedance.com>
2023-11-17 11:42:51 +0800

    enum 3rd: more complicated enum, and method for enum

--- ch6/enum_demo/src/main.rs
+++ ch6/enum_demo/src/main.rs
@@ -3,12 +3,29 @@ enum IpAddrKind {
     V6(String),
 }
 
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 }, // anonymous struct
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+// method on enum
+impl Message {
+    fn call(&self) {
+        println!("called an enum variant method");
+    }
+}
+
 fn main() {
     let four = IpAddrKind::V4(127, 0, 0, 1);
     let six = IpAddrKind::V6(String::from("::1"));
 
     route(four);
     route(IpAddrKind::V6(String::from("::1")));
+
+    let msg = Message::Write(String::from("hello"));
+    msg.call();
 }
 
 fn route(_ip_type: IpAddrKind) {
