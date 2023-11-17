b9afe1d939dff99e9ee79f432755b66ac1b89da5
huguanting.frank <huguanting.frank@bytedance.com>
2023-11-17 14:26:25 +0800

    option 1st: test Option enum, and it's difference between normal type

--- ch6/enum_demo/src/main.rs
+++ ch6/enum_demo/src/main.rs
@@ -1,3 +1,5 @@
+use std::option;
+
 enum IpAddrKind {
     V4(u8, u8, u8, u8),
     V6(String),
@@ -26,8 +28,27 @@ fn main() {
 
     let msg = Message::Write(String::from("hello"));
     msg.call();
+
+    // option test
+    option_test();
 }
 
 fn route(_ip_type: IpAddrKind) {
 
+}
+
+fn option_test() {
+    let some_number = Some(2);
+    let some_string = Some("a string");
+
+    let absent_number: Option<i32> = None;
+    // compile error: no type for option
+    // let absent_number = None;
+
+
+    // T and Option<T> are different types
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+    // compile error: mismatched types
+    // let sum = x + y;
 }
