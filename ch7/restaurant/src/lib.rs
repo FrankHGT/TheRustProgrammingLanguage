05dc3d80e47d52439e1c86ba7aba45da408af34c
Frank Hu <huguantingfrank@outlook.com>
2023-11-19 22:08:01 +0800

    ch7 2nd: absolute path, relative path, and pub keyword

--- ch7/restaurant/src/lib.rs
+++ ch7/restaurant/src/lib.rs
@@ -14,17 +14,65 @@ mod tests {
 }
 
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
 
-    mod serving {
+    pub mod serving {
         fn take_order() {}
 
-        fn serve_order() {}
+        pub fn serve_order() {}
 
         fn take_payment() {}
     }
+}
+
+mod back_of_house {
+    use std::arch::x86_64::_MM_FROUND_TO_NEAREST_INT;
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    // all variant is same with enum, pub -> pub private -> private
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches")
+            }
+        }
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::front_of_house::serving::serve_order();
+    }
+
+    fn cook_order() {}
+}
+
+pub fn eat_at_restaurant() {
+    // absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+    // relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // compile error cause seasonal_fruit is private
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
