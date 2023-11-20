8ea0fbdac55a70ab15cbc38e54849d483ba3b0cc
huguanting.frank <huguanting.frank@bytedance.com>
2023-11-20 17:12:42 +0800

    ch9 6th: ? operator can be called as chain

--- ch9/panic/src/main.rs
+++ ch9/panic/src/main.rs
@@ -47,22 +47,9 @@ fn propagate_error() {
 }
 
 fn read_username_from_file() -> Result<String, io::Error> {
-    let mut f= File::open("hello.txt")?;
-
-    // ? operator is same as logic below
-    // let mut f = match f {
-    //     Ok(file) => file,
-    //     Err(e) => return Err(e),
-    // };
-
     let mut s = String::new();
-    
-    // add ? operator, and Result will return early when error occur
-    f.read_to_string(&mut s)?;
-    Ok(s)
 
-    // match f.read_to_string(&mut s) {
-    //     Ok(_) => Ok(s),
-    //     Err(e) => Err(e),
-    // }
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+
+    Ok(s)
 }
