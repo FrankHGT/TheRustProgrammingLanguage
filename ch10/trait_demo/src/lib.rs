7bb021bd006642dd76e6f355bf70d3b3fe3a758e
huguanting.frank <huguanting.frank@bytedance.com>
2023-11-20 21:05:59 +0800

    ch10 3rd: trait constrains, impl syntactic sugar, multible constrains, where keyword

--- ch10/trait_demo/src/lib.rs
+++ ch10/trait_demo/src/lib.rs
@@ -1,3 +1,6 @@
+use core::fmt;
+use std::fmt::Display;
+
 pub trait Summary {
     fn summarize(&self) -> String {
         format!("(Read more from {}...)", self.summarize_author())
@@ -24,6 +27,12 @@ impl Summary for NewsArticle {
     }
 }
 
+impl Display for NewsArticle {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.summarize())
+    }
+}
+
 pub struct Tweet {
     pub username: String,
     pub content: String,
@@ -39,4 +48,32 @@ impl Summary for Tweet {
     fn summarize_author(&self) -> String {
         format!("@{}", self.username)
     }
+}
+
+// impl trait is just syntactic sugar
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+pub fn print_author<T: Summary>(item: &T) {
+    println!("Author of news: {}", item.summarize_author());
+}
+
+// item1, item2 can be different type, as long as they all impl Summary trait
+pub fn notify2(item1: &impl Summary, item2: &impl Summary) {
+    println!("Two new: {}, {}", item1.summarize(), item2.summarize());
+}
+
+// item1, item2 must be same type
+pub fn print_author2<T: Summary>(item1: &T, item2: &T) {
+    println!("Author of news 1: {}, news 2: {}", item1.summarize_author(), item2.summarize_author());
+}
+
+// multible trait constrain
+// no "impl" after "+"
+pub fn notify3(item: impl Summary + Display) {
+    println!("Display for item: {}", item);
+}
+pub fn print_author3<T: Summary + Display>(item: T) {
+    println!("Test")
 }
--- ch10/trait_demo/src/main.rs
+++ ch10/trait_demo/src/main.rs
@@ -1,4 +1,17 @@
-use trait_demo::{Tweet, Summary, NewsArticle};
+use std::fmt::{Display, Debug};
+
+use trait_demo::{Tweet, Summary, NewsArticle, notify, print_author, notify3};
+
+fn _some_function<T: Display + Clone, U: Clone + Debug>(_t: T, _u: U) -> i32 {
+    5
+}
+
+fn _some_function_where<T, U>(_t: T, _u: U) -> i32
+    where T: Display + Clone,
+        U: Clone + Debug
+{
+    5
+}
 
 fn main() {
     let tweet = Tweet {
@@ -9,6 +22,8 @@ fn main() {
     };
 
     println!("1 new tweet: {}", tweet.summarize());
+    notify(&tweet);
+    print_author(&tweet);
 
     let news = NewsArticle {
         headline: String::from("Headline"),
@@ -18,4 +33,7 @@ fn main() {
     };
 
     println!("default trait impl for NewsArticle: {}", news.summarize());
+    notify(&news);
+    print_author(&news);
+    notify3(news);
 }
