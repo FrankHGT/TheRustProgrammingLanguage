b621c05b2d792f7d24bfaeae66597ee5790ead01
huguanting.frank <huguanting.frank@bytedance.com>
2023-11-21 21:50:50 +0800

    ch11 1st: code for autotest, some useful marco and annotation

---
+++ ch11/adder/src/lib.rs
@@ -0,0 +1,88 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}", name)
+}
+
+mod guess;
+use guess::Guess;
+
+#[derive(Debug)]
+pub struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+impl Rectangle {
+    pub fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn exploration() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        // use Result to avoid panic
+        if 2 + 2 == 5 {
+            Ok(())
+        } else {
+            Err(String::from("2 + 2 != 4"))
+        }
+    }
+
+    // #[test]
+    // fn another() {
+    //     panic!("Make this test fail");
+    // }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle { length: 8, width: 7 };
+        let smaller = Rectangle { length: 5, width: 1 };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle { length: 8, width: 7 };
+        let smaller = Rectangle { length: 5, width: 1 };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Frank");
+        assert!(
+            result.contains("Frank"),
+            "Greeting did not contain name, value was {}", result
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+}
