98971cc846801469aebb065e79cc1a2635688d34
huguanting.frank <huguanting.frank@bytedance.com>
2023-11-22 14:53:05 +0800

    ch12 3rd: error handling -> cmd args parse will return a Result, and will call exit when args is invalid

--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -1,8 +1,13 @@
-use std::{env, fs};
+use std::{env, fs, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        // TODO: why unwrap_or_else need a Config for return, 
+        // but we can use exit to suppress the error?
+        process::exit(1);
+    });
 
     println!("Searching for \"{}\"", config.query);
     println!("In file {}", config.filename);
@@ -19,10 +24,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
