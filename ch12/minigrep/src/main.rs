bdc150cd96073ed4e8d3a28c35f3d0850a5ed7cb
huguanting.frank <huguanting.frank@bytedance.com>
2023-11-22 17:35:55 +0800

    ch12 8th: use eprintf output to stderr

--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -6,14 +6,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         // TODO: why unwrap_or_else need a Config for return, 
         // but we can use exit to suppress the error?
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application run error: {}", e);
+        eprintln!("Application run error: {}", e);
 
         process::exit(1);
     }
