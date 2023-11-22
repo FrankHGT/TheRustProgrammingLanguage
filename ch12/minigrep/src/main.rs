6a8ab36daadcf4b1a373cb5a65438f82384780d6
huguanting.frank <huguanting.frank@bytedance.com>
2023-11-22 15:08:28 +0800

    ch12 4th: move file read code to run function, and add error handling code for it

--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, process};
+use std::{env, fs, process, error::Error};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -12,10 +12,20 @@ fn main() {
     println!("Searching for \"{}\"", config.query);
     println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
+    if let Err(e) = run(config) {
+        println!("Application run error: {}", e);
+
+        process::exit(1);
+    }
+}
+
+// TODO: meaning of Box<dyn Error>
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
 
     println!("With text:\n{}", contents);
+
+    Ok(())
 }
 
 struct Config {
