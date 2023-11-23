543578ed3e8894067067f3cd456b49ae18b09b46
huguanting.frank <huguanting.frank@bytedance.com>
2023-11-23 11:25:27 +0800

    ch13 4th: use Fn trait construct a struct to store closure, thus reduce expensive calculation

--- ch13/keep/src/main.rs
+++ ch13/keep/src/main.rs
@@ -10,6 +10,35 @@ fn main() {
     );
 }
 
+struct Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
 fn generate_workout(intensity: u32, random_number: u32) {
     let expensive_closure = |num| {
         println!("calculating slowly...");
@@ -17,14 +46,16 @@ fn generate_workout(intensity: u32, random_number: u32) {
         num
     };
 
+    let mut cacher = Cacher::new(expensive_closure);
+
     if intensity < 25 {
         println!(
             "Today, do {} pushups!",
-            expensive_closure(intensity)
+            cacher.value(intensity)
         );
         println!(
             "Next, do {} situps!",
-            expensive_closure(intensity)
+            cacher.value(intensity)
         );
     } else {
         if random_number == 3 {
@@ -32,7 +63,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
         } else {
             println!(
                 "Today, run for {} minutes!",
-                expensive_closure(intensity)
+                cacher.value(intensity)
             );
         }
     }
